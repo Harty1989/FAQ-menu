e7383ce29f4e8d62516e2a00668281d6a7c33df2
Harty1989 <122306932+Harty1989@users.noreply.github.com>
2025-12-07 16:31:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+   base: "/Harty1989/",
   plugins: [
     tailwindcss(), // ← ONLY THIS, NOT "tailwindcss" or postcss plugins
     react(),

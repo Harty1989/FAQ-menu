f2c6f0b99f8f9a32c3e666fd4b15830a39c680bb
Harty1989 <122306932+Harty1989@users.noreply.github.com>
2025-12-07 16:33:51 +0100

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-   base: "/Harty1989/",
+   base: "/FAQ-menu/",
   plugins: [
     tailwindcss(), // ← ONLY THIS, NOT "tailwindcss" or postcss plugins
     react(),

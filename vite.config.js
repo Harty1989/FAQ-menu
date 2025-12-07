63b4202416a2d92d63859e71fb4b68188b532685
Harty1989 <122306932+Harty1989@users.noreply.github.com>
2025-12-07 16:37:13 +0100

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-   base: "/FAQ-menu/",
+ base: "/FAQ-menu/",
   plugins: [
     tailwindcss(), // ← ONLY THIS, NOT "tailwindcss" or postcss plugins
     react(),
